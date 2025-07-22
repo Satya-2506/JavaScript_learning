c8722f5c477de06c439c81af5e246c47780ef712
Satya Ranjan Mohanty <113417575+Satya-2506@users.noreply.github.com>
2025-07-22 12:28:29 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+//"use strict";  // Treat all JS code as newer JS version 
+
+// alert(3 + 3) // We are using nodeJS not browser, so can't directly use alert().
+
+console.log(3 
+    + 
+    3)   // Code readablility should be high
+
+console.log("Satya")
+
+
+let name = "Manisha"
+let age = 23
+let isLoggedIn = true
+
+
+// --- Premitive Datatypes ---
+// number => 
+// bigInt
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Manisha")  // string 
+console.log(typeof age);       // number
+
+console.log(typeof null)       // object
+console.log(typeof undefined); // undefined
